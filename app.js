a9c79f8852a38e493db3927c3db1010c5f519700
Ranadogac <dogac.rana1@icloud.com>
2025-12-29 00:16:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -84,8 +84,11 @@ passport.deserializeUser((user, done) => { done(null, user); });
 */
 // 4. E-POSTA AYARLARI
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+    service: 'gmail', // Bu bazen sorun çıkarır
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 // ==========================================
